98445a5af6be0ebdbff25311224a173fc022bc3f
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2024-01-02 08:44:56 +0700

    feat : home page

---
+++ components/auth/login-button.tsx
@@ -0,0 +1,23 @@
+'use client';
+import { useRouter } from 'next/navigation';
+
+type LoginButtonProps = {
+  children: React.ReactNode;
+  mode?: 'modal' | 'redirect';
+  asChild?: boolean;
+};
+
+export const LoginButton = ({ asChild, children, mode = 'redirect' }: LoginButtonProps) => {
+  const router = useRouter();
+  const onClick = () => {
+    router.push('/auth/login');
+  };
+  if (mode === 'modal') {
+    return <span>TODO: Implement modal</span>;
+  }
+  return (
+    <span className='cursor-pointer' onClick={onClick}>
+      {children}
+    </span>
+  );
+};
