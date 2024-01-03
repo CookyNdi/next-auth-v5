3eec5497cf43c4a097ddaa070165296db0aa0011
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2024-01-03 14:52:13 +0700

    feat : Resend (sending email verification)

--- actions/register.ts
+++ actions/register.ts
@@ -5,6 +5,8 @@ import bcrypt from 'bcryptjs';
 import { RegisterSchema } from '@/schemas';
 import { db } from '@/lib/db';
 import { getUserByEmail } from '@/data/user';
+import { generateVerificationToken } from '@/lib/tokens';
+import { sendVerificationEmail } from '@/lib/mail';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
@@ -23,9 +25,11 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
 
     await db.user.create({ data: { name, email, password: hashedPassword } });
 
-    // TODO : send verification token email
+    const verificationToken = await generateVerificationToken(email);
 
-    return { success: 'User created!' };
+    await sendVerificationEmail(verificationToken.email, verificationToken.token);
+
+    return { success: 'Confirmation email sent!' };
   } catch (error) {
     return { error: 'Internal server error!' };
   }
