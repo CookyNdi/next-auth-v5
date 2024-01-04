52b2db44e0216a643e15d35e45894900a302a020
CookyNdi <73926548+CookyNdi@users.noreply.github.com>
2024-01-04 12:24:53 +0700

    feat : reset password email

--- components/auth/reset-form.tsx
+++ components/auth/reset-form.tsx
@@ -3,10 +3,9 @@ import * as z from 'zod';
 import { useForm } from 'react-hook-form';
 import { useState, useTransition } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
-import Link from 'next/link';
 
-import { LoginSchema } from '@/schemas';
-import { login } from '@/actions/login';
+import { ResetSchema } from '@/schemas';
+import { reset } from '@/actions/reset';
 
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form';
 import { CardWrapper } from './card-wrapper';
@@ -14,40 +13,30 @@ import { Input } from '../ui/input';
 import { Button } from '../ui/button';
 import { FormError } from '../form-error';
 import { FormSuccess } from '../form-success';
-import { useSearchParams } from 'next/navigation';
 
-export const LoginForm = () => {
-  const searchParams = useSearchParams();
-  const urlError =
-    searchParams.get('error') === 'OAuthAccountNotLinked' ? 'Email already in use with different provider!' : '';
+export const ResetForm = () => {
   const [error, setError] = useState<string | undefined>();
   const [success, setSuccess] = useState<string | undefined>();
   const [isPending, startTransition] = useTransition();
 
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
+  const form = useForm<z.infer<typeof ResetSchema>>({
+    resolver: zodResolver(ResetSchema),
     defaultValues: {
       email: '',
-      password: '',
     },
   });
-  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+  const onSubmit = (values: z.infer<typeof ResetSchema>) => {
     setError('');
     setSuccess('');
     startTransition(() => {
-      login(values).then((data) => {
+      reset(values).then((data) => {
         setError(data?.error);
         setSuccess(data?.success);
       });
     });
   };
   return (
-    <CardWrapper
-      headerLabel='Wellcome Back'
-      backButtonLabel="Don't have an account?"
-      backButtonHref='/auth/register'
-      showSocial
-    >
+    <CardWrapper headerLabel='Forgot your password?' backButtonLabel='Back to login' backButtonHref='/auth/login'>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
           <div className='space-y-4'>
@@ -65,30 +54,11 @@ export const LoginForm = () => {
                 </FormItem>
               )}
             />
-            <FormField
-              control={form.control}
-              name='password'
-              disabled={isPending}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input {...field} placeholder='******' type='password' />
-                  </FormControl>
-                  <div className='w-full flex justify-end'>
-                    <Button size='sm' variant='link' asChild className='p-0 font-normal'>
-                      <Link href='/auth/reset'>Forgot Password?</Link>
-                    </Button>
-                  </div>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
           </div>
-          <FormError message={error || urlError} />
+          <FormError message={error} />
           <FormSuccess message={success} />
           <Button disabled={isPending} className='w-full' type='submit'>
-            Login
+            Send reset email
           </Button>
         </form>
       </Form>
